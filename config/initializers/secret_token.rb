5a2bd1841e2d9fce96bef2abda00a1b8e65d5882
ijones16 <ian@egghead.io>
2016-07-07 23:27:14 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WorkoutWarrior::Application.config.secret_key_base = '177327a5dc111bbe5b8fdc4e3525c81700db1969eef981d0fca01617a6c2f99be543a152587474bc4ca9b80f3e2b608681b8600fbf8a21c3926ef9763ac7a617'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+WorkoutWarrior::Application.config.session_store :cookie_store, key: '_workout-warrior_session'
