2f061265cca229293911b3813b5474d99e9e27b9
Ian Jones <jones58ian@gmail.com>
2016-07-08 06:42:56 +0000

    add workouts controller and index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 WorkoutWarrior::Application.routes.draw do
+
+  root 'workouts#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
